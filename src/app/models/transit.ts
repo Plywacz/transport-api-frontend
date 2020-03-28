e5ee5b98932a55d14380623d31885b2258b001a2
Plywacz <m.plywacz@interia.pl>
2020-03-28 12:08:50 +0100

    added transit dto class, and transit service + tiny refactoring

---
+++ src/app/models/transit.ts
@@ -0,0 +1,7 @@
+export class Transit {
+  id: number;
+  sourceAddress: string;
+  destinationAddress: string;
+  price: number;
+  date: string;
+}
---
+++ src/app/services/transit/transit.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Transit} from '../../models/transit';
+import {Observable} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransitService {
+  private readonly transitUrl: string = 'http://localhost:8080/api/transits/';
+
+  constructor(private http: HttpClient) {
+  }
+
+  public addTransit(transit: Transit): Observable<any> {
+    return this.http.post(this.transitUrl, transit);
+  }
+}
