2c914a8497bd17a5b41a03edbfe06a6fda85128b
Plywacz <m.plywacz@interia.pl>
2020-04-01 15:07:28 +0200

    added delete button, in every row of transit table that deletes selected row

--- src/app/components/display-driver/display-driver.component.ts
+++ src/app/components/display-driver/display-driver.component.ts
@@ -3,16 +3,19 @@ import {DriverService} from '../../services/driver/driver.service';
 import {AlertService} from '../../services/alert/alert.service';
 import {Driver} from '../../models/driver';
 import {ActivatedRoute, Router} from '@angular/router';
+import {TransitService} from '../../services/transit/transit.service';
 
 @Component({
   selector: 'app-driver-list',
   templateUrl: './display-driver.component.html',
   styleUrls: ['./display-driver.component.css']
 })
+//todo: sort transits by date
 export class DisplayDriverComponent implements OnInit {
   driver: Driver;
 
   constructor(private driverService: DriverService,
+              private transitService: TransitService,
               private alertService: AlertService,
               private route: ActivatedRoute,
               private router: Router) {
@@ -20,10 +23,12 @@ export class DisplayDriverComponent implements OnInit {
 
   ngOnInit(): void {
     const driverId = this.route.snapshot.params.id;
+    this.alertService.wait('Operation in progress');
 
     this.driverService.getDriver(driverId)
       .subscribe(data => {
           this.driver = data;
+          this.alertService.clear();
         },
         error => {
           const msg = ` ` + error.error.message || error.error || 'unknown Error';
@@ -32,4 +37,18 @@ export class DisplayDriverComponent implements OnInit {
         });
   }
 
+  deleteRow(transitId: number) {
+    if (confirm('Are you sure you want to delete this transit ???')) {
+      this.alertService.wait('Operation in progress');
+
+      this.transitService.deleteTransit(transitId)
+        .subscribe(succes => {
+            window.location.reload();
+          },
+          error => {
+            let msg = ' ' + error.error.message || error.error || 'unknown message';
+            this.alertService.error('Operation failed due to: ' + msg);
+          });
+    }
+  }
 }
